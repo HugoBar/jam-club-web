9f65f786c65fdda0f7f5008b090d96158aa1a266
Hugo Barreiro <hugomdbarreiro@gmail.com>
2025-02-27 14:01:14 +0000

    Misc

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ import GroupList from './groups/GroupList';
 
 function Home() {
   return (
-    <Box sx={{ width: '100%', maxWidth: 1000, margin: '0 auto' }}>
+    <Box sx={{ width: '100%', maxWidth: 1000, margin: '0 auto', paddingTop: "5%" }}>
       <DailyTrack />
 
       <GroupList />
