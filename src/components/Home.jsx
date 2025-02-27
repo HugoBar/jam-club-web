a43d755ff3cec4cb28b001126a76634f8bacc841
Hugo Barreiro <hugomdbarreiro@gmail.com>
2025-02-27 11:58:26 +0000

    Update Home with new components

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,42 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import UserInfo from './UserInfo'; // Import the UserInfo component
-import { useAuth } from './AuthProvider';
+import React from 'react';
+import { Box } from '@mui/material';
+import DailyTrack from './DailyTrack';
+import GroupList from './GroupList';
 
 function Home() {
-  const { userId, accessToken } = useAuth();
-  const [userDetails, setUserDetails] = useState(null);
-
-  useEffect(() => {
-    const fetchUserDetails = async () => {
-      try {
-        const response = await axios.get(`http://localhost:3000/users/${userId}`);
-        console.log('User details response:', response.data); // Add logging
-        setUserDetails(response.data);
-      } catch (error) {
-        console.error('Error fetching user details:', error);
-      }
-    };
-
-    if (userId) {
-      fetchUserDetails();
-    }
-  }, [userId, accessToken]);
-
   return (
-    <div>
-      <h1>Home Page</h1>
-      
-      {userDetails ? (
-        <div>
-          <UserInfo username={userDetails.username}/>
-          <p>{userDetails.username}</p>
-          <p>{accessToken}</p>
-        </div>
-      ) : (
-        <p>Loading user details...</p>
-      )}
-    </div>
+    <Box sx={{ width: '100%', maxWidth: 1000, margin: '0 auto' }}>
+      <DailyTrack />
+
+      <GroupList />
+    </Box>
   );
 }
 
