02c52c9708c2f2d38b13ccf78d498e7bf89d3e11
Hugo Barreiro <hugomdbarreiro@gmail.com>
2025-03-03 22:09:05 +0000

    Add Music Room

--- src/components/MusicRoom.jsx
+++ src/components/MusicRoom.jsx
@@ -1,56 +1,119 @@
-import React, { useState, useEffect } from 'react';
-import { getGroupById } from '../utils/groupRequests';
+import React, { useState, useEffect } from "react";
+import { getGroupById } from "../utils/groupRequests";
 import { useLocation } from "react-router-dom";
-import { Box, Typography} from '@mui/material';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { Box, Typography } from "@mui/material";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import SimpleSlider from "./recommendations/Slider";
+import UserRecommendation from "./recommendations/UserRecommendation";
+import { getGroupRecommendations } from "../utils/recommendationRequests";
+import GroupSettings from "./recommendations/GroupSettings";
+import GroupListTable from "./groups/GroupMembersList";
 
 const MusicRoom = () => {
   const [group, setGroup] = useState(null);
   const [loading, setLoading] = useState(true);
   const location = useLocation();
-  const groupId = location.state?.groupId; 
+  const [groupId, setGroupId] = useState(location.state?.groupId);
+  const [groupRecommendations, setGroupRecommendations] = useState(null);
 
-  const fetchGroup = async (groupId) => {
+  const fetchGroupRecommendations = async () => {
     try {
-      console.log('Fetching group details:', groupId);
-      const response = await getGroupById(groupId);
-      console.log(response)
-      setGroup(response);
-      setLoading(false); 
+      const response = await getGroupRecommendations(groupId);
+      setGroupRecommendations(response);
     } catch (error) {
-      console.error('Error fetching user details:', error);
-      setGroup(null); 
-      setLoading(false); 
+      console.error("Error fetching group recommendation:", error);
+    }
+  };
+
+  const fetchGroup = async () => {
+    try {
+      const response = await getGroupById(groupId);
+      setGroup(response);
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching user details:", error);
+      setGroup(null);
+      setLoading(false);
     }
   };
 
   useEffect(() => {
-    fetchGroup(groupId);
-  }, []);
+    fetchGroup();
+    fetchGroupRecommendations();
+  }, [groupId]);
 
   if (loading) {
     return <div>Loading...</div>;
   }
 
   return (
-    <Box sx={{ 
-      width: '100%', 
-      maxWidth: 1000, 
-      margin: '0 auto', 
-      paddingTop: "5%", 
-      display: 'flex', 
-      alignItems: 'center',
-    }}>
-      <ArrowBackIcon 
-        sx={{ cursor: 'pointer', marginRight: 1, marginTop: 4 }} 
-        onClick={() => window.history.back()} 
-      />
-      <Typography variant="h4" sx={{ fontWeight: 'bold', textAlign: 'left', marginTop: 4 }}>
-        {group.name}
-      </Typography>
-    </Box>
-    
+    <>
+      <Box
+        sx={{
+          width: "100%",
+          maxWidth: "1000px",
+          margin: "0 auto",
+          paddingTop: "5%",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <ArrowBackIcon
+              sx={{ cursor: "pointer", marginRight: 1 }}
+              onClick={() => window.history.back()}
+            />
+            <Typography variant="h4" sx={{ fontWeight: "bold" }}>
+              {group.name}
+            </Typography>
+          </Box>
+
+          <GroupSettings group={group} />
+        </Box>
+        <Box>
+          <UserRecommendation
+            groupId={groupId}
+            onNewRecommendation={fetchGroupRecommendations}
+          />
+          <SimpleSlider items={groupRecommendations} />
+        </Box>
+      </Box>
+
+      <Box
+        sx={{
+          width: "300px",
+          position: "fixed",
+          top: "10%",
+          right: 0,
+          marginRight: "5%",
+          boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+          padding: "20px",
+          backgroundColor: "#fff",
+          display: { xs: "none", sm: "block" },
+        }}
+      >
+        <GroupListTable group={group} onNewMemberList={fetchGroup} />
+      </Box>
+
+      <Box
+        sx={{
+          width: "100%",
+          padding: "20px",
+          display: { xs: "block", sm: "none" },
+          marginTop: "20px",
+        }}
+      >
+        <GroupListTable group={group} />
+      </Box>
+    </>
   );
-}
+};
 
 export default MusicRoom;
